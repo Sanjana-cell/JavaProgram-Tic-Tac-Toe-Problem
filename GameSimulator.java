a9168acb9bcc1a3989a3e42293596a72a5490cbd
Sanjana K R <sanjanakr97@gmail.com>
2020-08-02 21:47:42 +0530

    Added: player starts with fresh board

---
+++ GameSimulator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sanjana K R
+ *
+ */
+public class GameSimulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[][] board = new int[3][3]; 
+
+	}
+
+}
