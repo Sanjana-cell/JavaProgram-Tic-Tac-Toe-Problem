43e48379664db7c2acfbf36f5dab8d3833e8e844
Sanjana K R <sanjanakr97@gmail.com>
2020-08-02 22:00:19 +0530

    Added: toss to check player or computer plays first

--- GameSimulator.java
+++ GameSimulator.java
@@ -1,10 +1,10 @@
-/**
- * 
- */
+import java.util.Random;
 
 /**
  * @author Sanjana K R
- *
+ * Date: 8-2-2020
+ * Compilation Command: javac GameSimulator.java
+ * Execution Command: java GameSimulator
  */
 public class GameSimulator {
 
@@ -12,8 +12,13 @@ public class GameSimulator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[][] board = new int[3][3]; 
-
+		int[][] board = new int[3][3];
+		Random random = new Random();
+		boolean toss=random.nextBoolean();
+		if(toss == true)
+			System.out.println("Player turn");
+		else
+			System.out.println("Computer turn");
 	}
 
 }
